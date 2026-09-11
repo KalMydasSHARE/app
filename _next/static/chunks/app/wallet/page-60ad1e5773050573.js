a7c7d67a9d727cf68bc46e699d74ecff04075394
kalmydas-cloud <kalmydas@gmail.com>
2026-09-11 21:59:39 +0200

    deploy frontend app.kalmydas.com

--- _next/static/chunks/app/wallet/page-60ad1e5773050573.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3597],{90332:function(n,e,u){Promise.resolve().then(u.bind(u,15199))}},function(n){n.O(0,[3169,7319,959,713,29,5156,1067,8631,3833,7048,5199,2971,2117,1744],function(){return n(n.s=90332)}),_N_E=n.O()}]);
