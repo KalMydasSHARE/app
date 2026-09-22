7627788a2083212e045b2021b9e3474d4db3e4a5
kalmydas-cloud <kalmydas@gmail.com>
2026-09-22 13:19:35 +0200

    deploy frontend app.kalmydas.com

--- _next/static/chunks/8374.d22dc40478b2ddac.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8374],{88374:function(e,n,u){u.r(n),u.d(n,{default:function(){return f}});var t=u(57437),r=u(80148);function f(){return(0,t.jsx)(r.default,{integre:!1})}}}]);
